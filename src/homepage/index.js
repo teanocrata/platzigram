a77cc0bb08c5869913f5b706891aad3d21ad12cb
teanocrata <teanocrata@gmail.com>
2016-12-28 17:31:00 +0100

    20 Supuesto timeline para los usuarios

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -1,8 +1,10 @@
 var page = require('page');
+var empty = require('empty-element');
 var title = require('title');
+var template = require('./template');
 
 page('/', function(ctx, next) {
     title('Platzigram');
     var main = document.getElementById('main-container');
-    main.innerHTML = '<a href="/signup">Signup</a>';
+    empty(main).appendChild(template);
 });
--- src/signin/template.js
+++ src/signin/template.js
@@ -8,7 +8,7 @@ var signinform = yo`<div class="col s12 m7">
             <form class="signup-form">
                 <div class="section">
                     <a class="btn btn-fb hide-on-small-only">Iniciar sesión con Facebook</a>
-                    <a class="btn btn-fb hide-on-med-and-up">Iniciar sesión</a>
+                    <a class="btn btn-fb hide-on-med-and-up"><i class="fa fa-facebook-official" aria-hidden="true"></i> Iniciar sesión</a>
                 </div>
                 <div class="divider"></div>
                 <div class="section">
