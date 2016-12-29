1b973ac5be77631ad423dd8f9e054ae1234f507a
teanocrata <teanocrata@gmail.com>
2016-12-29 11:04:04 +0100

    22 funcionalidad de like

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -6,5 +6,26 @@ var title = require('title');
 page('/', function(ctx, next) {
     title('Platzigram');
     var main = document.getElementById('main-container');
-    empty(main).appendChild(template);
+
+    var pictures = [{
+        user: {
+            username: 'teanocrata',
+            avatar: 'https://pbs.twimg.com/profile_images/1227097907/Sv6FQ2MO'
+        },
+        url: 'office.jpg',
+        likes: 10,
+        liked: true
+    },
+    {
+        user: {
+            username: 'teanocrata',
+            avatar: 'https://pbs.twimg.com/profile_images/1227097907/Sv6FQ2MO'
+        },
+        url: 'office.jpg',
+        likes: 2,
+        liked: true
+    }
+  ];
+
+    empty(main).appendChild(template(pictures));
 });
---
+++ src/picture-card/index.js
@@ -0,0 +1,39 @@
+var yo = require('yo-yo');
+
+module.exports = function pictureCard( pic) {
+
+  var el;
+
+  function render(picture) {
+      return yo `<div class="card ${picture.liked ? 'liked' : ''}">
+      <div class="card-image">
+        <img class="activator" src="${picture.url}">
+      </div>
+      <div class="card-content">
+        <a href="/user/${picture.user.username}" class="card-title">
+          <img src="${picture.user.avatar}" class="avatar" />
+          <spam class="username">${picture.user.username}</span>
+        </a>
+        <small class="right time">Hace 1 día</small>
+        <p>
+          <a class="left" href="#" onclick=${like.bind(null, true)}><i class="fa fa-heart-o" aria-hidden="true"></i></a>
+          <a class="left" href="#" onclick=${like.bind(null, false)}><i class="fa fa-heart" aria-hidden="true"></i></a>
+          <span class="left likes">${picture.likes} me gusta</span>
+        </p>
+      </div>
+    </div>`;
+
+  }
+
+  function like(liked){
+    pic.liked= liked;
+    pic.likes+= liked ? 1 : -1;
+    var newEl= render(pic);
+    yo.update(el, newEl);
+    return false;
+  }
+
+  el = render(pic);
+
+  return el;
+};
