000c0a05bb52ae25b411265b5dbbceb0ec2e43a4
teanocrata <teanocrata@gmail.com>
2016-12-30 14:21:41 +0100

    32 Agregando un botón para subir una nueva foto

--- src/translate/en-US.js
+++ src/translate/en-US.js
@@ -16,5 +16,7 @@ module.exports = {
     'signup.have-account': 'Already have an account?',
     'signin': 'Signin',
     'signin.not-have-account': 'Don\'t have an account?',
-    'language': 'Language'
+    'language': 'Language',
+    'upload-picture': 'Upload picture',
+    'upload': 'Subir'
 };
