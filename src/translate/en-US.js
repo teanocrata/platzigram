c50ac7fad5174872a8b67c987d9b128dc0f19cee
teanocrata <teanocrata@gmail.com>
2016-12-30 10:22:23 +0100

    27 traducción completa

--- src/translate/en-US.js
+++ src/translate/en-US.js
@@ -1,5 +1,20 @@
 module.exports = {
-  likes: '{likes, plural, ' +
-            '=1 {# like}' +
-            'other {# likes}}'
+    likes: '{likes, plural, ' +
+        '=1 {# like}' +
+        'other {# likes}}',
+    logout: 'Logout',
+    english: 'English',
+    spanish: 'Spanish',
+    'signup.subheading': 'Signup to watch your friends\' pictures studying at Platzi',
+    'signup.facebook': 'Signup with Facebook',
+    'signup.text': 'Signup',
+    email: 'Email',
+    username: 'Username',
+    fullname: 'Fullname',
+    password: 'Password',
+    'signup.call-to-action': 'Signup',
+    'signup.have-account': 'Already have an account?',
+    'signin': 'Signin',
+    'signin.not-have-account': 'Don\'t have an account?',
+    'language': 'Language'
 };
