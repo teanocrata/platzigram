4cd21b8fd31edb1c592709d478136218622c6ef9
teanocrata <teanocrata@gmail.com>
2017-01-09 09:57:45 +0100

    Solución platzi primer desafio

--- src/user/index.js
+++ src/user/index.js
@@ -1,22 +1,23 @@
-var page = require('page');
-var empty = require('empty-element');
-var template = require('./template');
-var title = require('title');
+import page from 'page'
+import header from '../header'
+import title from 'title'
+import empty from 'empty-element'
+import template from './template'
+
 var request = require('superagent');
-var header = require('../header');
+
 require('whatwg-fetch');
 
-page('/:userId', header, asyncLoad, function(ctx, next) {
+page('/:userId', header, loadUser, function(ctx, next) {
     title(ctx.user.fullname);
     var main = document.getElementById('main-container');
 
-    empty(main).appendChild(template(ctx.user, ctx.pictures));
+    empty(main).appendChild(template(ctx.user));
 });
 
-async function asyncLoad(ctx, next) {
+async function loadUser(ctx, next) {
     try {
         ctx.user = await fetch('/api/users/' + ctx.params.userId).then(res => res.json());
-        ctx.pictures = await fetch('/api/pictures').then(res => res.json());
         next();
     } catch (err) {
         console.log(err);
--- src/user/template.js
+++ src/user/template.js
@@ -1,47 +1,54 @@
-var yo = require('yo-yo');
-var layout = require('../layout');
-var picture = require('../picture-card');
-var translate = require('../translate');
-var request = require('superagent');
+import yo from 'yo-yo'
+import layout from '../layout'
+import picture from '../picture-card'
+import translate from '../translate'
+import request from 'superagent'
 
-module.exports = function(user, pictures){
- var el = yo `<article class="container">
-    <header class="row valign-wrapper">
-      <div class="col s4">
-        <img class="responsive-img circle profile-avatar" src="${user.avatar}"/>
-      </div>
-      <div class="col s8">
-        <div class="row valign-wrapper">
-          <div class="col s9">
-          <h1 class="truncate profile-username">${user.username}</h1>
-          </div>
-          <div class="col s2">
-          <a class="waves-effect waves-light btn transparent grey-text z-depth-0">${translate.message('edit-profile')}</a>
-          </div>
-          <div class="col s1">
-          <a class="waves-effect waves-light btn transparent grey-text z-depth-0"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></a>
-          </div>
-        </div>
-        <div class="row valign-wrapper">
-          <div class="col s4 likes">${translate.message('posts', {posts: user.posts})}</div>
-          <div class="col s4 likes">${translate.message('follower', {follower: user.follower})}</div>
-          <div class="col s4 likes">${translate.message('following', {following: user.following})}</div>
-        </div>
-        <div class="row">
-          <h2 class="profile-fullname">${user.fullname}</h2>
-        </div>
-      </div>
-    </header>
+export default function userPageTemplate(user){
+  var el = yo `<article class="container user-page">
     <div class="row">
-        ${pictures.map(function(pic){
-          return grid(picture(pic));
-        })}
+      <header class="col s12 m10 offset-m1 l8 offset-l2 center-align heading">
+        <div class="row">
+          <div class="col s12 m10 offset-m1 l3 offse-l3 center">
+            <img class="responsive-img circle profile-avatar" src="${user.avatar}"/>
+          </div>
+          <div class="col s12 m10 offset-m1 l6 left-align">
+            <div class="row valign-wrapper">
+              <div class="col s9">
+                <h1 class="truncate profile-username">${user.username}</h1>
+              </div>
+              <div class="col s2">
+                <a class="waves-effect waves-light btn transparent grey-text z-depth-0">${translate.message('edit-profile')}</a>
+              </div>
+              <div class="col s1">
+                <a class="waves-effect waves-light btn transparent grey-text z-depth-0"><i class="fa fa-ellipsis-h" aria-hidden="true"></i></a>
+              </div>
+            </div>
+            <div class="row valign-wrapper">
+             <div class="col s4 likes">${translate.message('posts', {posts: user.posts})}</div>
+             <div class="col s4 likes">${translate.message('follower', {follower: user.follower})}</div>
+             <div class="col s4 likes">${translate.message('following', {following: user.following})}</div>
+            </div>
+            <div class="row">
+             <h2 class="profile-fullname">${user.fullname}</h2>
+            </div>
+          </div>
+        </div>
+      </header>
+      <div class="col">
+        <div class="row">
+          ${user.pictures.map(function(picture){
+            return yo`<div class="col s12 m6 l4">
+              <div class="picture-container">
+                <img src=${picture.url} class="picture"/>
+                <div class="likes"><i class="fa fa-heart"></i> ${picture.likes}</div>
+              </div>
+            </div>`;
+          })}
+        </div>
+      </div>
     </div>
   </article>`;
-
-  function grid(content){
-    return yo`<div class="col s4">${content}</div>`;
-  }
 
   function cancel(){
     toggleButtons();
@@ -63,14 +70,14 @@ module.exports = function(user, pictures){
 
     var data = new FormData(this);
     request
-      .post('/api/pictures')
-      .send(data)
-      .end(function (err, res){
-        console.log(arguments);
-      })
-
+    .post('/api/pictures')
+    .send(data)
+    .end(function (err, res){
+     console.log(arguments);
+    })
   }
 
 
   return layout(el);
-};
+
+}
